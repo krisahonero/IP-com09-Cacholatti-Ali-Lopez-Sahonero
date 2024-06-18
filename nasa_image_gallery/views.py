f48537653dfb96247e3d0e85c28c8fcf7b8046ac
Nahuel <nahuelnano06@gmail.com>
2024-06-18 11:29:55 -0300

    funciones de view.py y la del buscador

--- nasa_image_gallery/views.py
+++ nasa_image_gallery/views.py
@@ -12,8 +12,9 @@ def index_page(request):
 
 # auxiliar: retorna 2 listados -> uno de las imágenes de la API y otro de los favoritos del usuario.
 def getAllImagesAndFavouriteList(request):
-    images = []
+    images = services_nasa_image_gallery.getAllImages()
     favourite_list = []
+   
 
     return images, favourite_list
 
@@ -23,6 +24,7 @@ def home(request):
     # (*) este último, solo si se desarrolló el opcional de favoritos; caso contrario, será un listado vacío [].
     images = []
     favourite_list = []
+    images,favourite_list=getAllImagesAndFavouriteList(request)
     return render(request, 'home.html', {'images': images, 'favourite_list': favourite_list} )
 
 
@@ -30,9 +32,13 @@ def home(request):
 def search(request):
     images, favourite_list = getAllImagesAndFavouriteList(request)
     search_msg = request.POST.get('query', '')
+    if search_msg!="":
+        return render(request, 'home.html', {'images':services_nasa_image_gallery.getImagesBySearchInputLike(search_msg)  } )
+    else:
+        return(home(request)) 
 
     # si el usuario no ingresó texto alguno, debe refrescar la página; caso contrario, debe filtrar aquellas imágenes que posean el texto de búsqueda.
-    pass
+
 
 
 # las siguientes funciones se utilizan para implementar la sección de favoritos: traer los favoritos de un usuario, guardarlos, eliminarlos y desloguearse de la app.
