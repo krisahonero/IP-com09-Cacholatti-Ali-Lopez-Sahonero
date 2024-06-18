292766a1cc32caf6d238bd2bcecf334384b97448
Nahuel <nahuelnano06@gmail.com>
2024-06-18 11:24:12 -0300

    funcion de get all images

--- nasa_image_gallery/layers/services/services_nasa_image_gallery.py
+++ nasa_image_gallery/layers/services/services_nasa_image_gallery.py
@@ -9,8 +9,11 @@ def getAllImages(input=None):
     # obtiene un listado de imágenes desde transport.py y lo guarda en un json_collection.
     # ¡OJO! el parámetro 'input' indica si se debe buscar por un valor introducido en el buscador.
     json_collection = []
+    json_collection=transport.getAllImages(input)
 
     images = []
+    for i in range(0,len(json_collection)):
+        images.append(mapper.fromRequestIntoNASACard(json_collection[i]))
 
     # recorre el listado de objetos JSON, lo transforma en una NASACard y lo agrega en el listado de images. Ayuda: ver mapper.py.
 
